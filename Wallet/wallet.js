1cb456cb6ac6454448168d6f5ce7462cdd9f0dd2
Ivo Spasov <ivospasov89@gmail.com>
2018-03-04 20:51:09 +0200

    Implemented the functionality to open an existing wallet by private key.

--- Wallet/wallet.js
+++ Wallet/wallet.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-    function GenerateRandomWallet() {
-        let ec = new elliptic.ec('secp256k1');
-        let ripemd160 = new Hashes.RMD160();
+    let ec = new elliptic.ec('secp256k1');
+
+    function generateRandomWallet() {
         let keyPair = ec.genKeyPair();
+        return getWalletFromKeyPair(keyPair);
+    }
+
+    function openExistingWallet(privateKey) {
+        let keyPair = ec.keyFromPrivate(privateKey);
+        return getWalletFromKeyPair(keyPair);
+    }
+
+    function getWalletFromKeyPair(keyPair) {
+        let ripemd160 = new Hashes.RMD160();
         let privateKey = keyPair.getPrivate().toString('hex');
         let publicPoint = keyPair.getPublic();
         let publicKey = publicPoint.encode('hex');
@@ -24,11 +34,26 @@ $(document).ready(function () {
     }
 
     $('#new-wallet').click(() => {
-        let wallet = GenerateRandomWallet();
+        let wallet = generateRandomWallet();
         saveWalletToLocalStorage(wallet);
+        $('#private-key').empty();
+        $('#public-key').empty();
+        $('#address').empty();
         $('#private-key').append(wallet.privateKey);
         $('#public-key').append(wallet.publicKey);
         $('#address').append(wallet.address);
     });
 
+    $('#open-wallet').click(() => {
+        let privateKey = $('#private-key-input').val();
+        let wallet = openExistingWallet(privateKey);
+        saveWalletToLocalStorage(wallet);
+        $('#private-key-input').val('');
+        $('#private-key2').empty();
+        $('#public-key2').empty();
+        $('#address2').empty();
+        $('#private-key2').append(wallet.privateKey);
+        $('#public-key2').append(wallet.publicKey);
+        $('#address2').append(wallet.address);
+    });
 });
