2fc2f22ad99b9ec353636db95110d4a784946ed2
Ivo Spasov <ivospasov89@gmail.com>
2018-03-04 19:46:57 +0200

    Generated a public and a private key.

--- Wallet/wallet.js
+++ Wallet/wallet.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    console.log('Hello.');
+    function GenerateRandomWallet() {
+        let ec = new elliptic.ec('secp256k1');
+        let keyPair = ec.genKeyPair();
+        let privateKey = keyPair.getPrivate().toString('hex');
+        let publicPoint = keyPair.getPublic();
+        var publicKey = publicPoint.encode('hex');    
+        
+        console.log(privateKey);
+        console.log(publicKey);
+    }
+
+    GenerateRandomWallet();
 });
