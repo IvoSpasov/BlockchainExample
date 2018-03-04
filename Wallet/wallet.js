149fadc2aa0d1733059886ac491f02de425a0af2
Ivo Spasov <ivospasov89@gmail.com>
2018-03-04 20:03:07 +0200

    Added hashes library and calculated the address using ripemd160

--- Wallet/wallet.js
+++ Wallet/wallet.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
     function GenerateRandomWallet() {
         let ec = new elliptic.ec('secp256k1');
+        let ripemd160 = new Hashes.RMD160();
         let keyPair = ec.genKeyPair();
         let privateKey = keyPair.getPrivate().toString('hex');
         let publicPoint = keyPair.getPublic();
-        var publicKey = publicPoint.encode('hex');    
+        let publicKey = publicPoint.encode('hex'); 
+        let address = ripemd160.hex(publicKey);
         
         console.log(privateKey);
         console.log(publicKey);
+        console.log(address);
     }
 
     GenerateRandomWallet();
