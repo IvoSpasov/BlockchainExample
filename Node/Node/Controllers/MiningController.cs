b47411e977158b2e56326a1a8abe08b1b183d4d6
Ivo Spasov <ivospasov89@gmail.com>
2018-03-07 21:04:51 +0200

    Added a mining controller.

---
+++ Node/Node/Controllers/MiningController.cs
@@ -0,0 +1,24 @@
+﻿namespace Node.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Node.Interfaces;
+
+    [Produces("application/json")]
+    [Route("api/Mining")]
+    public class MiningController : Controller
+    {
+        private INodeService nodeService;
+
+        public MiningController(INodeService nodeService)
+        {
+            this.nodeService = nodeService;
+        }
+
+        [HttpGet("get-mining-job/{minerAddress}")]
+        public JsonResult GetNextMiningJob(string minerAddress)
+        {
+            this.nodeService.ProcessNextMiningJob(minerAddress);
+            return Json("123");
+        }
+    }
+}
